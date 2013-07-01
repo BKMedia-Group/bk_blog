80a1e064905e941853695b46d4cd84772e59e39e
Josh Horner <oushinhogin@gmail.com>
2013-07-01 13:44:40 -0600

    adds external blank linking to markdown parser

--- lib/core-ext/string.rb
+++ lib/core-ext/string.rb
@@ -1,6 +1,17 @@
+class RedcarpetWithExternalLinks < Redcarpet::Render::HTML
+  def link(link, title, content)
+    if content[0] == '_'
+      "<a href='#{link}' title='#{title}' target='_blank'>#{content[1..-1]}</a>"
+    else
+      "<a href='#{link}' title='#{title}'>#{content}</a>"
+    end
+  end
+end
 class String
   def to_html
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true, :tables => true)
+
+
+    markdown = Redcarpet::Markdown.new(RedcarpetWithExternalLinks, :autolink => true, :space_after_headers => true, :tables => true)
     markdown.render(self).html_safe
   end
 end
